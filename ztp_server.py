272fd9a55a93cbe479a6a656f656ec3406e2619a
paul.freitag@gmail.com <paul.freitag@gmail.com>
2022-04-28 16:25:36 +0200

    First commit

---
+++ ztp_server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from flask import render_template
+import yaml
+import os
+
+# This is the directory where a yaml file for every device should be located.
+#ansible_host_dir = "/home/paul/ENAUI/ansible/host_vars/"
+ansible_host_dir = "/home/paul/ENAUI/ansible/host_vars"
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Info coming soon'
+
+
+@app.route('/get-config')
+def get_config():
+
+   device = request.args.get('device')
+
+   if device is None:
+       return "No device input given" 
+   else:
+       data = device.split(',') 
+
+       for filename in os.listdir(ansible_host_dir):
+           with open(os.path.join(ansible_host_dir, filename), 'r') as file: 
+               device = yaml.load(file, Loader=yaml.FullLoader)
+
+           if device['ztp_sn'] == data[1][3:]:
+               return render_template("template_" + device['ztp_template']+".tmpl",device_data=device)
+
+       return "!# Serial -" + data[1][3:] + "- not found !"
+
+@app.route('/ztp.py')
+def ztp():
+    return render_template('template_ztp_script.tmpl',server=request.host)
+
+
+@app.route('/ztp3.py')
+def ztp3():
+    return render_template('template_ztp_script_3.tmpl',server=request.host)
